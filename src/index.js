46bf92ea63637f666103d5aa89b552cb6a1da582
Alka Sah <alkasah485@gmail.com>
2024-07-12 13:34:50 -0700

    changes saved

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const registrationForm = document.getElementById("registration");
+console.log(registrationForm);
+const loginForm  = document.getElementById("login");
+console.log(loginForm);
+
+const userName = registrationForm.elements["username"];
+console.log(userName);
+
+console.log("hello");
